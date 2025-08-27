1ecd8edbb42bbf216dd065256d1fe8392782377e
adriansaape <adriansaape@gmail.com>
2025-08-27 11:14:05 -0500

    Se completa la fase RED

--- test_calculadora.py
+++ test_calculadora.py
@@ -4,4 +4,7 @@ def test_suma_simple():
     assert evaluar_expresion("2+3") == 5
 
 def test_orden_operaciones():
-    assert evaluar_expresion("2+3*4") == 14  # 3*4=12 → 2+12=14
+    assert evaluar_expresion("2+3*4") == 14
+
+def test_resta_simple():
+    assert evaluar_expresion("5-2") == 3 # Esta es la nueva prueba
